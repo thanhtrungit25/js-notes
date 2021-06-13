f7540d6eb6814a5983c79cc119046a9fc207a300
Trung DG <thanhtrung.it25@gmail.com>
2021-06-13 16:54:34 +0700

    Trick esbuild css handle, escape css

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -24,20 +24,34 @@ export const fetchPlugin = (inputCode: string) => {
         // Config cache
         // Check to see if we have alreay fetched this file
         // and if it is in the cache
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        )
+        // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+        //   args.path
+        // )
 
-        // if it is return it immediately
-        if (cachedResult) {
-          return cachedResult
-        }
+        // // if it is return it immediately
+        // if (cachedResult) {
+        //   return cachedResult
+        // }
 
         const { data, request } = await axios.get(args.path)
 
+        const fileType = args.path.match(/.css$/) ? 'css' : 'jsx'
+        const escaped = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'")
+        const contents =
+          fileType === 'css'
+            ? `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `
+            : data
+
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
-          contents: data,
+          contents,
           resolveDir: new URL('./', request.responseURL).pathname
         }
         // store response in cache
