dfe97aa89c8a889b4ac1b33bb34d2f90e969d6db
Trung DG <thanhtrung.it25@gmail.com>
2021-06-13 15:11:58 +0700

    Add IndexedDB lib to cache many request

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm'
 import axios from 'axios'
+import localForage from 'localforage'
+
+const fileCache = localForage.createInstance({
+  name: 'filecache'
+})
 
 export const unpkgPathPlugin = () => {
   return {
@@ -41,13 +46,29 @@ export const unpkgPathPlugin = () => {
           }
         }
 
+        // Config cache
+        // Check to see if we have alreay fetched this file
+        // and if it is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        )
+
+        // if it is return it immediately
+        if (cachedResult) {
+          return cachedResult
+        }
+
         const { data, request } = await axios.get(args.path)
-        console.log(request)
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname
         }
+        // store response in cache
+        await fileCache.setItem(args.path, result)
+
+        return result
       })
     }
   }
