c4775c89d5c5309f5ed58203064fde7c2bddf242
Trung DG <thanhtrung.it25@gmail.com>
2021-06-13 14:09:45 +0700

    Resoving nested paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,11 +9,21 @@ export const unpkgPathPlugin = () => {
         console.log('onResole', args)
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' }
-        } else if (args.path === 'tiny-test-pkg') {
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
           return {
-            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
-            namespace: 'a'
+            namespace: 'a',
+            path: new URL(
+              args.path,
+              'https://unpkg.com' + args.resolveDir + '/'
+            ).href
           }
+        }
+
+        return {
+          path: `https://unpkg.com/${args.path}`,
+          namespace: 'a'
         }
       })
 
@@ -25,17 +35,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              import message from 'nested-test-pkg';
               console.log(message);
             `
           }
         }
 
-        const { data } = await axios.get(args.path)
-        console.log(data)
+        const { data, request } = await axios.get(args.path)
+        console.log(request)
         return {
           loader: 'jsx',
-          contents: data
+          contents: data,
+          resolveDir: new URL('./', request.responseURL).pathname
         }
       })
     }
