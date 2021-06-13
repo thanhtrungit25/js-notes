87f75a039a51fd256b2bea157faa69d5b58c86d5
Trung DG <thanhtrung.it25@gmail.com>
2021-06-13 13:07:05 +0700

    Add plugin to bundle with simple package

---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,43 @@
+import * as esbuild from 'esbuild-wasm'
+import axios from 'axios'
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResole', args)
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' }
+        } else if (args.path === 'tiny-test-pkg') {
+          return {
+            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace: 'a'
+          }
+        }
+      })
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args)
+
+        if (args.path === 'index.js') {
+          // Don't load the file system we will load for you
+          return {
+            loader: 'jsx',
+            contents: `
+              import message from 'tiny-test-pkg';
+              console.log(message);
+            `
+          }
+        }
+
+        const { data } = await axios.get(args.path)
+        console.log(data)
+        return {
+          loader: 'jsx',
+          contents: data
+        }
+      })
+    }
+  }
+}
