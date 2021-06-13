34b890c4c36f7b0646fba61e7b66e410e2eef44b
Trung DG <thanhtrung.it25@gmail.com>
2021-06-13 22:20:23 +0700

    Pub, sub message to iframe and test render react component

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
   const ref = useRef<any>()
+  const iframe = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
@@ -25,6 +26,8 @@ const App = () => {
       return
     }
 
+    iframe.current.srcdoc = html
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -38,8 +41,30 @@ const App = () => {
 
     // console.log(result)
 
-    setCode(result.outputFiles[0].text)
+    // setCode(result.outputFiles[0].text)
+
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
   }
+
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root"></div>
+      </body>
+      <script>
+        window.addEventListener('message', (event) => {
+          try {
+            eval(event.data)
+          } catch (err) {
+            const root = document.querySelector('#root')
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+            console.error(err)
+          }
+        }, false)
+      </script>
+    </html>
+  `
 
   return (
     <div>
@@ -51,6 +76,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} />
     </div>
   )
 }
