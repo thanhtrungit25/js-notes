84e9e77995d915f3d496b6992844bacf0f13777d
Trung DG <thanhtrung.it25@gmail.com>
2021-06-14 14:31:00 +0700

    Add monaco editor & update changed to esbuild

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,40 @@
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react'
+
+interface CodeEditorProps {
+  initialValue: string
+  onChange(value: string): void
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const onEditorDidMount: EditorDidMount = (getEditorValue, monacoEditor) => {
+    monacoEditor.onDidChangeModelContent(() => {
+      onChange(getEditorValue())
+    })
+
+    monacoEditor.getModel()?.updateOptions({ tabSize: 2 })
+  }
+
+  return (
+    <MonacoEditor
+      editorDidMount={onEditorDidMount}
+      value={initialValue}
+      theme='dark'
+      language='javascript'
+      height='500px'
+      options={{
+        wordWrap: 'on',
+        minimap: {
+          enabled: false
+        },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
+      }}
+    />
+  )
+}
+
+export default CodeEditor
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import { fetchPlugin } from './plugins/fetch-plugin'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import CodeEditor from './components/code-editor'
 
 const App = () => {
   const ref = useRef<any>()
@@ -68,6 +69,10 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor
+        initialValue='const a = 2;'
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
