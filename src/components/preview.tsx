87d5bf3bd4686cc284fcdda59f98ec40850207ca
Trung DG <thanhtrung.it25@gmail.com>
2021-06-15 15:26:23 +0700

    Capturing and print bundling errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react'
 
 interface PreviewProps {
   code: string
+  err: string
 }
 
 const html = `
@@ -36,7 +37,7 @@ const html = `
     </html>
   `
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
   const iframe = useRef<any>()
 
   useEffect(() => {
@@ -49,6 +50,8 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
     }, 50)
   }, [code])
 
+  console.log(err)
+
   return (
     <div className='preview-wrapper'>
       <iframe
@@ -57,6 +60,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         sandbox='allow-scripts'
         srcDoc={html}
       />
+      {err && <div className='preview-error'>{err}</div>}
     </div>
   )
 }
