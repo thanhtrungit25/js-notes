71a5f229a567576467c314d01c16cb74095184b1
Trung DG <thanhtrung.it25@gmail.com>
2021-06-15 22:30:39 +0700

    Delete cell, move cell logic

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -27,8 +27,22 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       state.data[id].content = content
       return
     case ActionType.DELETE_CELL:
-      return state
+      delete state.data[action.payload]
+      state.order.filter((id) => id !== action.payload)
+
+      return
     case ActionType.MOVE_CELL:
+      const { direction } = action.payload
+      const index = state.order.findIndex((id) => id === action.payload.id)
+      const targetIndex = direction === 'up' ? index - 1 : index + 1
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return
+      }
+
+      state.order[index] = state.order[targetIndex]
+      state.order[targetIndex] = action.payload.id
+
       return state
     case ActionType.INSERT_CELL_BEFORE:
       return state
