e82f06dd7f0b32980cc3e4b8199a0177168f31f7
Trung DG <thanhtrung.it25@gmail.com>
2021-06-15 21:24:36 +0700

    Setup redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import TextEditor from './components/text-editor'
+import { store } from './state'
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   )
 }
 
