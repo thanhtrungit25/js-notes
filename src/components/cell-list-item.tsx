7bf00b9aaa03d07ab5cd4bd0f79180f2339d1caf
Trung DG <thanhtrung.it25@gmail.com>
2021-06-16 14:59:46 +0700

    Move action-bar to right with custom css

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,3 +1,4 @@
+import './cell-list-item.css'
 import React from 'react'
 import { Cell } from '../state'
 import CodeCell from './code-cell'
@@ -17,9 +18,9 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   }
 
   return (
-    <div>
-      <ActionBar id={cell.id} />
+    <div className='cell-list-item'>
       {child}
+      <ActionBar id={cell.id} />
     </div>
   )
 }
