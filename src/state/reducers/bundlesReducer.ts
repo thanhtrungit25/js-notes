c4cee15a6fb7a40b839b499a729575a925ccd8d5
Trung DG <thanhtrung.it25@gmail.com>
2021-06-18 11:39:05 +0700

    Change bundle reducer process, render Preview

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -3,36 +3,39 @@ import { ActionType } from '../action-types'
 import { Action } from '../actions'
 
 interface BundlesState {
-  [key: string]: {
-    loading: boolean
-    code: string
-    err: string
-  }
+  [key: string]:
+    | {
+        loading: boolean
+        code: string
+        err: string
+      }
+    | undefined
 }
 
 const initialState: BundlesState = {}
 
-const reducer = produce(
-  (state: BundlesState = initialState, action: Action): BundlesState => {
+const reducer = (
+  state: BundlesState = initialState,
+  action: Action
+): BundlesState => {
+  return produce(state, (draft) => {
     switch (action.type) {
       case ActionType.BUNDLE_START:
-        state[action.payload.cellId] = {
+        draft[action.payload.cellId] = {
           loading: true,
           code: '',
           err: ''
         }
-        return state
+        break
       case ActionType.BUNDLE_COMPLETE:
-        state[action.payload.cellId] = {
+        draft[action.payload.cellId] = {
           loading: false,
           code: action.payload.bundle.code,
           err: action.payload.bundle.err
         }
-        return state
-      default:
-        return state
+        break
     }
-  }
-)
+  })
+}
 
 export default reducer
